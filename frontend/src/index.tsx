45ed796527e6e2f6043eab518ee1c284a019f3bd
TatyanaDev <kot.kate2020@gmail.com>
2021-11-25 16:27:51 +0200

    create react app

---
+++ frontend/src/index.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
