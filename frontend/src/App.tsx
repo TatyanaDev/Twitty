291f6dd11ec3d862a423a633a49fb69fd334070a
TatyanaDev <kot.kate2020@gmail.com>
2021-11-29 18:26:05 +0200

    Refactor component src

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,32 @@
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import { useEffect } from "react";
+import PostsListGuest from "./components/PostListGuest";
 import PostsList from "./components/PostList";
+import Messages from "./components/Messages";
+import Settings from "./components/Settings";
+import Profile from "./components/Profile";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import Users from "./components/Users";
 
 export default function App() {
+  useEffect(() => {
+    checkOnAuth();
+  }, []);
+
+  const checkOnAuth = () => localStorage.getItem("registration_token");
+
   return (
     <section>
       <Switch>
-        <Route exact path='/' component={PostsList} />
+        <Route exact path='/' render={() => (checkOnAuth() ? <PostsList /> : <PostsListGuest />)} />
         <Route exact path='/login' component={SignIn} />
         <Route exact path='/register' component={SignUp} />
+        <Route exact path='/messages' component={Messages} />
+        <Route exact path='/users' component={Users} />
+        <Route exact path='/profile' component={Profile} />
+        <Route exact path='/settings' component={Settings} />
       </Switch>
-      {/* <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/messages'>Messages</Link>
-          </li>
-          <li>
-            <Link to="/users">Users</Link>
-          </li>
-          <li>
-            <Link to={"/add"}>Profile</Link>
-          </li>{" "}
-          <li>
-            <Link to={"/add"}>Settings</Link>
-          </li>
-        </ul>
-      </nav> */}
     </section>
   );
 }
