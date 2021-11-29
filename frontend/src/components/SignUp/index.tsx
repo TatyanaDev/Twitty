fcaa828588044a95a47bfc4c37fd5129a3632189
TatyanaDev <kot.kate2020@gmail.com>
2021-11-29 18:22:27 +0200

    Refactor component sign up

--- frontend/src/components/SignUp/index.tsx
+++ frontend/src/components/SignUp/index.tsx
@@ -1,53 +1,64 @@
-import ButtonsSign from "../ButtonsSign";
+import { Link, useHistory } from "react-router-dom";
+import { useState } from "react";
+import UserDataService from "../../services/user.service";
+import style from "./styles.module.css";
 
 export default function SignUp() {
-  // const createUser = (event: any) => {
-  //   event.preventDefault();
+  const [confirmationError, setConfirmationError] = useState<boolean>(false);
 
-  //   PostDataService.createPost({ content: event.target[0].value })
-  //     .then(({ data }: any) => {
-  //       setPosts({ data: "[data.data, ...posts?.data]" });
-  //       event.target[0].value = "";
-  //     })
-  //     .catch((err: Error) => {
-  //       console.error(err);
-  //     });
-  // };
+  const history = useHistory();
 
-  const createUser = () => {};
+  const createUser = (event: any) => {
+    event.preventDefault();
 
-  const changeInputRegister = () => {};
+    UserDataService.createUser({ firstName: event.target[0].value, lastName: event.target[1].value, userName: event.target[2].value, email: event.target[3].value, password: event.target[4].value })
+      .then(({ data }: any) => {
+        localStorage.setItem("registration_token", data.token);
+        if (data.token) {
+          history.push("/");
+        }
+      })
+      .catch((err: Error) => {
+        console.error(err);
+      });
+  };
+
+  const closeConfirmationError = () => {
+    setConfirmationError(false);
+  };
 
   return (
     <section>
       <h1>Sign Up</h1>
       <form onSubmit={createUser}>
-        <label>
-          First name
-          <input type='text' name='name' onChange={changeInputRegister} />
-        </label>
-        <label>
-          Last name
-          <input type='text' name='name' onChange={changeInputRegister} />
-        </label>
-        <label>
-          User name
-          <input type='text' name='name' onChange={changeInputRegister} />
-        </label>
-        <label>
-          Email
-          <input type='email' name='name' onChange={changeInputRegister} />
-        </label>
-        <label>
-          Password
-          <input type='password' name='name' onChange={changeInputRegister} />
-        </label>
-        <label>
-          Password confirmation
-          <input type='password' name='name' onChange={changeInputRegister} />
-        </label>
+        <label className={style.label}>First name </label>
+        <input type='text' />
+        <label className={style.label}>Last name </label>
+        <input type='text' />
+        <label className={style.label}>User name </label>
+        <input type='text' />
+        <label className={style.label}>Email </label>
+        <input type='email' />
+        <label className={style.label}>Password </label>
+        <input type='password' />
+        <label className={style.label}>Password confirmation</label>
+        <input type='password' />
+        {confirmationError && (
+          <p>
+            Password mismatch<button onClick={closeConfirmationError}>X</button>
+          </p>
+        )}
+        <nav>
+          <ul >
+            <li>
+              <button type='submit'>Sign up</button>
+            </li>
+            <li>
+              <Link to='/login'>Sign in</Link>
+            </li>
+          </ul>
+        </nav>
       </form>
-      <ButtonsSign />
     </section>
   );
 }
