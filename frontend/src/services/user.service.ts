46989edf8a1ca369637bca8963cf3fa5e907e813
TatyanaDev <kot.kate2020@gmail.com>
2021-11-29 18:21:53 +0200

    Added  service check user

--- frontend/src/services/user.service.ts
+++ frontend/src/services/user.service.ts
@@ -2,11 +2,16 @@ import http from "../http-common";
 import IUserData from "../types/User";
 
 const createUser = (data: IUserData) => {
-  return http.post<IUserData>("/user", data);
+  return http.post<IUserData>("/user/register", data);
+};
+
+const checkUser = (data: IUserData) => {
+  return http.post<IUserData>("/user/login", data);
 };
 
 const UserService = {
   createUser,
+  checkUser,
 };
 
 export default UserService;
