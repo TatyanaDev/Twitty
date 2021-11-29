d39db196899a28dc0af854ddbc25f62843b3bcba
TatyanaDev <kot.kate2020@gmail.com>
2021-11-29 18:23:55 +0200

    Added component sign in

--- frontend/src/components/SignIn/index.tsx
+++ frontend/src/components/SignIn/index.tsx
@@ -1,3 +1,28 @@
+import { Link, useHistory } from "react-router-dom";
+import styles from "./styles.module.css";
+
 export default function SignIn() {
-  return <>авторизация</>;
+  const checkUser = () => {};
+
+  return (
+    <section>
+      <h1>Sign In</h1>
+      <form onSubmit={checkUser}>
+        <label className={styles.label}>User name (email)</label>
+        <input type='text' />
+        <label className={styles.label}>Password </label>
+        <input type='password' />
+        <nav>
+          <ul>
+            <li>
+              <button type='submit'>Sign in</button>
+            </li>
+            <li>
+              <Link to='/register'>Sign up</Link>
+            </li>
+          </ul>
+        </nav>
+      </form>
+    </section>
+  );
 }
