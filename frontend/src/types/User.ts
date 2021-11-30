1cf68ba6301e0b381832ab59276b889b9030cd93
TatyanaDev <kot.kate2020@gmail.com>
2021-11-30 17:24:38 +0200

    Fix interface IUserData

--- frontend/src/types/User.ts
+++ frontend/src/types/User.ts
@@ -1,8 +1,8 @@
 export default interface IUserData {
   id?: any | null;
-  firstName: string;
-  lastName: string;
-  userName: string;
+  firstName?: string;
+  lastName?: string;
+  userName?: string;
   email: string;
   password: string;
   createdAt?: Date;
