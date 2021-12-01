d2044ecc81afe70b973aee9b573395a16d3c409b
TatyanaDev <kot.kate2020@gmail.com>
2021-12-01 17:20:25 +0200

    Added user and posts routes

--- backend/routes/post.ts
+++ backend/routes/post.ts
@@ -4,6 +4,6 @@ const PostController = require("../controller/post.ts");
 const postRouter: any = RouterForPosts();
 
 postRouter.route("/").post(PostController.createPost).get(PostController.getPosts);
-postRouter.route("/:id").patch(PostController.updatePost).delete(PostController.deletePost);
+postRouter.route("/:postId").patch(PostController.updatePost).delete(PostController.deletePost);
 
 module.exports = postRouter;
--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -1,9 +1,13 @@
 const { Router: RouterForUsers } = require("express");
+const UserAndPostsController = require("../controller/userAndPosts");
 const UserController = require("../controller/user.ts");
 
 const userRouter: any = RouterForUsers();
 
 userRouter.route("/register").post(UserController.createUser);
 userRouter.route("/login").post(UserController.checkUser);
+
+userRouter.route("/:userId/post").post(UserAndPostsController.createPostForUser).get(UserAndPostsController.getUserPosts);
+userRouter.route("/:userId/post/:postId").patch(UserAndPostsController.updateUserPost).delete(UserAndPostsController.deleteUserPost);
 
 module.exports = userRouter;
