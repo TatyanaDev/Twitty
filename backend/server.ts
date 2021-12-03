1af916d330742b3e004a7a12b8b5aa05e733012c
TatyanaDev <kot.kate2020@gmail.com>
2021-12-03 13:09:01 +0200

    Fix server

--- backend/server.ts
+++ backend/server.ts
@@ -4,4 +4,10 @@ const application = require("./app.ts");
 const server: any = http.createServer(application);
 const port: string | number = process.env.PORT || 5000;
 
-server.listen(port, () => console.log(`Server started on port: ${port}`));
+(function () {
+  try {
+    server.listen(port, () => console.log(`Server started on port: ${port}`));
+  } catch (err) {
+    console.error(err);
+  }
+})();
