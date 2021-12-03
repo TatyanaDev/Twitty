5c11a71f8d1a8e25e2346d6054fc6a6a969e6780
TatyanaDev <kot.kate2020@gmail.com>
2021-12-03 13:31:42 +0200

    Added auth middleware

---
+++ backend/middlewares/auth.ts
@@ -0,0 +1,30 @@
+export {};
+const { verifyAccessToken } = require("../service/token");
+const ApiError = require("../exceptions/apiError");
+
+module.exports = function (req: any, res: any, next: any) {
+  try {
+    const authorizationHeader = req.headers.authorization;
+
+    if (!authorizationHeader) {
+      return next(ApiError.UnauthorizedError());
+    }
+
+    const accessToken = authorizationHeader.split(" ")[1];
+
+    if (!accessToken) {
+      return next(ApiError.UnauthorizedError());
+    }
+
+    const userData = verifyAccessToken(accessToken);
+
+    if (!userData) {
+      return next(ApiError.UnauthorizedError());
+    }
+
+    req.userData = userData;
+    next();
+  } catch (err) {
+    return next(ApiError.UnauthorizedError());
+  }
+};
