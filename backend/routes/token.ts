ca0cbecd34c1e6d4b080f51569e38a6a6664080f
TatyanaDev <kot.kate2020@gmail.com>
2021-12-03 13:29:25 +0200

    Added validator for incoming data

--- backend/routes/token.ts
+++ backend/routes/token.ts
@@ -1,11 +1,16 @@
 export {};
+const { body } = require("express-validator");
 const { Router } = require("express");
 const TokenController = require("../controller/token");
 
 const tokenRouter: any = Router();
 
-tokenRouter.get("/refresh", TokenController.refresh);
+tokenRouter.get("/refresh", 
+            TokenController.refresh);
 
-tokenRouter.post("/logout", TokenController.logout);
+tokenRouter.post("/logout",
+            body("email").isString().notEmpty().isEmail(), 
+            body("password").isString().isLength({ min: 6 }),  
+            TokenController.logout);
 
 module.exports = tokenRouter;
--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -1,13 +1,40 @@
-const { Router: RouterForUsers } = require("express");
+export {};
+const { body } = require("express-validator");
+const { Router } = require("express");
 const UserAndPostsController = require("../controller/userAndPosts");
 const UserController = require("../controller/user.ts");
+const authMiddleware = require("../middlewares/auth")
 
-const userRouter: any = RouterForUsers();
+const userRouter: any = Router();
 
-userRouter.route("/register").post(UserController.createUser);
-userRouter.route("/login").post(UserController.checkUser);
+userRouter.post("/register", 
+           body("firstName").isString().notEmpty(), 
+           body("lastName").isString().notEmpty(), 
+           body("userName").isString().notEmpty(), 
+           body("email").isString().notEmpty().isEmail(), 
+           body("password").isString().isLength({ min: 6 }), 
+           UserController.createUser);
 
-userRouter.route("/:userId/post").post(UserAndPostsController.createPostForUser).get(UserAndPostsController.getUserPosts);
-userRouter.route("/:userId/post/:postId").patch(UserAndPostsController.updateUserPost).delete(UserAndPostsController.deleteUserPost);
+userRouter.post("/login", 
+           body("email").isString().notEmpty().isEmail(), 
+           body("password").isString().isLength({ min: 6 }), 
+           UserController.checkUser);
+
+userRouter.get("/data",
+           authMiddleware, 
+           UserController.getUserInfo);
+
+userRouter.route("/:userId/post")
+          .get(authMiddleware,
+           UserAndPostsController.getUserPosts)
+          .post(authMiddleware,
+           body("content").isString().notEmpty(), 
+           UserAndPostsController.createPostForUser)
+
+userRouter.route("/:userId/post/:postId")
+          .patch(authMiddleware,
+           UserAndPostsController.updateUserPost)
+          .delete(authMiddleware,
+           UserAndPostsController.deleteUserPost);
 
 module.exports = userRouter;
