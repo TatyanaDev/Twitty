93913d373df420dec7e1c6b7d5d93e54c5b49022
TatyanaDev <kot.kate2020@gmail.com>
2021-12-09 16:08:48 +0200

    Fix page Nome

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -10,14 +10,14 @@ import UserService from "../../services/user.service";
 import IPostData from "../../types/Post";
 
 export default function Home({ userData, setUserData }: any) {
-  const initialValues = {
-    content: "",
-  };
-
   const [posts, setPosts] = useState<IPostData[]>([]);
   const [currentPost, setCurrentPost] = useState<null>(null);
   const [content, setContent] = useState<string>("");
   const history = useHistory();
+
+  const initialValues = {
+    content: "",
+  };
 
   useEffect(() => {
     getUserData();
