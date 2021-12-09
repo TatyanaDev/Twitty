a575b69f522fd25461bcb6318e90f5c548e2099f
TatyanaDev <kot.kate2020@gmail.com>
2021-12-09 14:21:08 +0200

    Fix inner pages

--- frontend/src/pages/Inner/Settings/index.tsx
+++ frontend/src/pages/Inner/Settings/index.tsx
@@ -1,9 +1,29 @@
+import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
 import NavigationMenu from "../../../components/NavigationMenu";
+import UserService from "../../../services/user.service";
 
-export default function Settings({ userData }: any) {
+export default function Settings({ userData, setUserData }: any) {
+  const history = useHistory();
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  const getUserData = async () => {
+    try {
+      const { data } = await UserService.getUserData();
+
+      setUserData(data.data);
+    } catch (err) {
+      localStorage.removeItem("token");
+
+      history.push("/");
+    }
+  };
   return (
     <section className='container'>
-     {userData && <NavigationMenu userData={userData} />}
+      {userData && <NavigationMenu userData={userData} />}
       <p>Settings</p>
     </section>
   );
