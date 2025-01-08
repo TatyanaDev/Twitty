0a02008e2434d96b0665c2a4916b9a15b64d0483
TatyanaDev <kot.kate2020@gmail.com>
2025-01-08 10:32:03 +0100

    small fixes

---
+++ frontend/src/api/index.ts
@@ -0,0 +1,66 @@
+import axios, { AxiosRequestConfig, AxiosError } from "axios";
+import AuthService from "../services/auth.service";
+import { BASE_URL } from "../config";
+
+interface CustomAxiosRequestConfig extends AxiosRequestConfig {
+  _isRetry?: boolean;
+}
+
+const api = axios.create({
+  headers: {
+    "Content-type": "application/json",
+  },
+  baseURL: `${BASE_URL}/api`,
+  withCredentials: true,
+});
+
+api.interceptors.request.use((config: CustomAxiosRequestConfig) => {
+  if (!config.headers) {
+    config.headers = {};
+  }
+
+  const token = localStorage.getItem("accessToken");
+
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error: AxiosError) => {
+    const originalRequest: CustomAxiosRequestConfig = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._isRetry) {
+      originalRequest._isRetry = true;
+
+      try {
+        const { data } = await AuthService.refresh();
+
+        localStorage.setItem("accessToken", data.data.accessToken);
+
+        if (!originalRequest.headers) {
+          originalRequest.headers = {};
+        }
+
+        originalRequest.headers["Authorization"] = `Bearer ${data.data.accessToken}`;
+
+        return api.request(originalRequest);
+      } catch (err) {
+        console.error("Failed to refresh token:", err);
+
+        localStorage.removeItem("accessToken");
+
+        window.location.href = "/";
+
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
